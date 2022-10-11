b800d5d0b10eb0a475b3f69505f50e73937d5b8d
Gonzalo Ariel Rossi <gonzaloarielrossi@gmail.com>
2022-10-11 15:49:51 -0300

    fixed responsive design issues

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -9,17 +9,36 @@ function Cta() {
       <div
         className={
           isMobile
-            ? 'box columns --cta-color has-text-light --border ml-0 mr-0 mt-0 mb-0'
-            : 'box columns --cta-color has-text-light --border ml-0 mr-0 mt-0 mb-0 --pad-0'
+            ? 'box columns --cta-color has-text-light --border m-0'
+            : 'box columns --cta-color has-text-light --border m-0 --pad-0'
+          // : 'box columns --cta-color has-text-light --border ml-0 mr-0 mt-0 mb-0 --pad-0'
         }
       >
-        <div className="column has-text-weight-bold has-text-centered is-size-3 --cta-pading mt-6 mb-6">
+        <div
+          className={
+            isMobile
+              ? 'column has-text-weight-bold has-text-centered is-size-3 --cta-pading my-0'
+              : 'column has-text-weight-bold has-text-centered is-size-3 --cta-pading my-6'
+          }
+        >
           Contact Me
         </div>
-        <div className="column has-text-centered is-size-4 --cta-pading is-align-self-center mt-6 mb-6">
+        <div
+          className={
+            isMobile
+              ? 'column has-text-centered is-size-4 --cta-pading is-align-self-center my-0'
+              : '"column has-text-centered is-size-4 --cta-pading is-align-self-center my-6"'
+          }
+        >
           <p>{"Like what you see? Let's talk!"}</p>
         </div>
-        <div className="column has-text-centered --cta-pading mt-6 mb-6">
+        <div
+          className={
+            isMobile
+              ? 'column has-text-centered --cta-pading my-0'
+              : 'column has-text-centered --cta-pading my-6'
+          }
+        >
           <Link to="/contact">
             <button className="button is-outlined is-info has-text-weight-bold is-size-5 --border --hover">
               👉 Send me a message
